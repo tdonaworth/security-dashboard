00b388860aeac99db24ed7685bd93dabd649f2a1
Tim Donaworth <tim.donaworth@flexion.hcqis.org>
2020-06-17 13:38:52 -0400

    minor cleanup

--- swagger/getOpenApiJson.py
+++ swagger/getOpenApiJson.py
@@ -2,9 +2,10 @@ import requests
 import os
 import json
 import re
-from github import Github
+#from github import Github
 
 def main():
+    # Need to work out the proper repo for this, as each repo gives different results (user or org) 
     url = "https://git.hcqis.org/api/v3/user/repos?type=all"
     #url = "https://git.hcqis.org/api/v3/orgs/iQIES/repos"
 
@@ -33,18 +34,16 @@ def write_services(input = ()):
     dir = "/swagger"
     f = open(os.getcwd() + dir + "/services.txt", "w")
     for i in input:
-        name = i#.replace('iqies', '').replace('service', '').replace('-','').strip()
-        print(name)
+        name = i.replace('iqies', '').replace('service', '').replace('-','').strip()
+        #print(name)
         f.write(name + os.linesep)
 
 def write_openapis(input = ()):
-    #kongToken=`curl 'https://test2-iqies.hcqis.org/api/auth/v1/public/login' -H 'Content-Type: application/json' --data '{"username":"ro-region1","password":"sdfsdff"}' | 
-    # #python3.5 -c "import sys, json; print(json.load(sys.stdin)['kongToken'])"`
     for i in input:
         service = i.replace('iqies', '').replace('service', '').replace('-','').strip()
-        print(service)
+        #print(service)
         url = "https://test2-iqies.hcqis.org/api/{}/openapi.json".format(service)
-        print(url)
+        #print(url)
         payload = {}
         kongToken = authenticate()
         headers = {'Content-Type': 'application/json'}
@@ -74,24 +73,9 @@ if __name__ == "__main__":
     main()
 
 
+# Tried a different path using the PyGithub library, but it was making writing back out difficult.
+
 """
-
-    url="https://test2-iqies.hcqis.org/api/${ii}/openapi.json";
-	echo $url;
-	output_file_tmp=$(dirname $0)/"${ii}.tmp";
-	output_file_json=$(dirname $0)/"${ii}.json";
-	echo "Writing json to " $output_file_tmp;
-	#echo $kongToken
-	curl -H "Cookie:iqies-token=${kongToken}" $url > $output_file_tmp
-	python3.5 -mjson.tool $output_file_tmp 
-	if [ $? -eq 0 ]
-	then
-		echo $output_file_tmp " is json format"
-		mv $output_file_tmp $output_file_json
-	else
-		echo $output_file_tmp " was not json" >&2
-	fi
-
 def main():
     RPATTERN = '^iqies.*service$'
     TIME_FORMAT = "%m/%d/%y, %H:%M:%S"
@@ -109,15 +93,4 @@ def main():
 
     jsonServices = json.dumps(services)
     print(jsonServices)
-
-
-class Service(object):
-    def __init__(self, name, full_name, created_at, updated_at, git_url, openapi):
-        self.name = name
-        self.full_name = full_name
-        self.created_at = created_at
-        self.updated_at = updated_at
-        self.git_url = git_url
-        self.openapi = openapi
-        
- """
+"""
