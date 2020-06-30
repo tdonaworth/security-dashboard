84e87147321d2a5d5c9c091f7c1c0449d28905f9
Tim Donaworth <tim.donaworth@flexion.hcqis.org>
2020-06-30 13:31:08 -0400

    added user and login stubs

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from redis import Redis
+import rq
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.redis = Redis.from_url(app.config['REDIS_URL'])
+app.task_queue = rq.Queue('sec-dash-tasks', connection = app.redis)
+
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 import json
 import os
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -21,3 +22,21 @@ def services():
 @app.route('/nexus')
 def nexus():
     return render_template('nexus.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/user/<username>')
+#@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    tasks=  [
+        {'id': 'task123', 'name':'task1', 'description':'a background job', 'complete':True},
+        {'id': 'task124', 'name':'task2', 'description':'still a background job', 'complete':False}
+    ]
+    return runder_template('user.html', user=user, tasks=tasks)
