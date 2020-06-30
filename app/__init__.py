d76763965726b3104cbcc12d0df75d2076fa39dc
Tim Donaworth <tim.donaworth@flexion.hcqis.org>
2020-06-30 10:52:09 -0400

    migrate and db fix

--- app/__init__.py
+++ app/__init__.py
@@ -9,4 +9,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+### Small work-around for sqlite issues on 'ALTER' see https://github.com/miguelgrinberg/Flask-Migrate/issues/61
+with app.app_context():
+    if db.engine.url.drivername == 'sqlite':
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+
+
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -6,7 +6,11 @@ class Task(db.Model):
     id          = db.Column(db.String(36), primary_key=True)
     name        = db.Column(db.String(128), index=True)
     description = db.Column(db.String(128), default='')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     complete    = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<Task {}>'.format(self.name)
 
     def get_rq_job(self):
         try:
@@ -18,3 +22,15 @@ class Task(db.Model):
     def get_progress(self):
         job = self.get_rq_job()
         return job.meta.get('progress', 0) if job is not None else 100
+
+
+
+class User(db.Model):
+    id            = db.Column(db.Integer, primary_key=True)
+    username      = db.Column(db.String(64), index=True, unique=True)
+    email         = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
