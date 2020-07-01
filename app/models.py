06c2d479bca6ff0b47c99e17b629bc703b657974
Tim Donaworth <tim.donaworth@flexion.hcqis.org>
2020-06-30 21:00:15 -0400

    nightly

--- app/models.py
+++ app/models.py
@@ -1,10 +1,14 @@
 import redis
 import rq
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(64), index=True, unique=True)
     email         = db.Column(db.String(120), index=True, unique=True)
@@ -15,7 +19,7 @@ class User(db.Model):
         return '<User {}>'.format(self.username) 
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(hash(password))
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
