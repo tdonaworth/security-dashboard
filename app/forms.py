84ff09e6d7c12620439b4962966548cd0fc456aa
wolfkabal <wolfk@Yoda-1.bioserge.lan>
2020-06-30 23:12:14 -0400

    added user registration

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, Email, EqualTo
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username    = StringField('Username', validators=[DataRequired()])
@@ -13,5 +14,14 @@ class RegistrationForm(FlaskForm):
     username    = StringField('Username', validators=[DataRequired()])
     email       = StringField('Email', validators=[DataRequired(), Email()])
     password    = PasswordField('Password', validators=[DataRequired()])
-    password2   = PasswordField('Repeat Password', validators=[DataRequired()], EqualTo('password')])
+    password2   = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit      = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Plase use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        raise ValidationError('That email is already registered to a user.')
